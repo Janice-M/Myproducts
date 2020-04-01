7ddbfae36ce013954576c8893ffd55c9b1b18d0e
Janice-M <janiceink001@gmail.com>
2020-04-01 21:11:49 +0300

    logs working

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+console.log(34);
 
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
