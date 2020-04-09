a5c665ca241bd5768e65aa4e6a47103a46961fd5
Janice-M <janiceink001@gmail.com>
2020-04-09 23:14:26 +0300

    properly linked

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log(34
-
 
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
